f434a6f7338495a0557a8c0ba248d4e94361ba71
Nate Del Duca <Nate@localhost.localdomain>
2016-10-05 12:10:22 -0400

    new file:   Gulpfile.js 	new file:   README-INSTRUCTIONS.txt 	new file:   site/css/style.css 	new file:   site/css/style.sass 	new file:   site/img/nated2.png 	new file:   site/index.html 	new file:   site/jade/body.jade 	new file:   site/jade/header.jade 	new file:   site/jade/index.jade 	new file:   site/jade/scripts.jade 	new file:   site/js/all.js 	new file:   site/js/js-lib/functions.js 	new file:   site/js/js-lib/jquery.js

---
+++ README-INSTRUCTIONS.txt
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////
+/// you must install these first in this order for gulp to work
+\\\ Make sure NODE.JS IS INSTALLED
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+/// run these commands in the root of your project
+
+npm init
+npm install gulp gulp-sass gulp-jade gulp-pug gulp-concat browser-sync gulp-autoprefixer --save-dev
---
+++ site/js/js-lib/functions.js
@@ -0,0 +1,23 @@
+//$(window).scroll(
+//    function(){pageScroll(); }
+    //setInterval(function(){arrowBounce()},2000);
+//);
+
+//function pageScroll () {
+//  var wScroll = $(window).scrollTop();
+//  console.log(wScroll);
+//}
+
+//blahasdfsadf
+//asdfds
+//fdsa
+//nathana
+$(window).scroll(
+    function(){pageScroll(); }
+    setInterval(function(){arrowBounce()},2000);
+);
+
+function pageScroll () {
+  var wScroll = $(window).scrollTop();
+  console.log(wScroll);
+}
